64deb1ffca2153c43e4d512dd62b82ef09fd53d8
SUMIT <40803138+sumitnce1@users.noreply.github.com>
2020-07-18 22:25:05 +0530

    Add files via upload

---
+++ 2swap2no.c
@@ -0,0 +1,14 @@
+//Q2. Write a program to Swapping Two Number.
+
+#include<stdio.h>  
+ void main()    
+{    
+int a=10, b=20;  
+    
+printf("Before swap a=%d b=%d",a,b);      
+a=a+b;    
+b=a-b;    
+a=a-b;    
+printf("\nAfter swap a=%d b=%d",a,b);    
+  
+}   
