ec7f92ab749a22feeb4aebf83f7888a8043b8c81
SUMIT <40803138+sumitnce1@users.noreply.github.com>
2020-07-18 22:25:24 +0530

    Add files via upload

---
+++ add2no.c
@@ -0,0 +1,15 @@
+//Q1. Write a program to Addition of Two Number.
+ 
+#include<stdio.h>
+ 
+void main() 
+{
+   int a, b, sum;
+ 
+   printf("\nEnter two no: ");
+   scanf("%d %d", &a, &b);
+ 
+   sum = a + b;
+ 
+   printf("Sum : %d", sum);
+}
